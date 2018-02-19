e0d7ff132d97aea3fabd75cd6d9cf30c33855dd0
Ravi Onteddu <ravio@Ravis-MacBook-Pro.local>
2018-02-20 00:01:46 +1100

    Initial commit

---
+++ src/test/java/fullpagecapture/DriverDefinition.java
@@ -0,0 +1,9 @@
+package fullpagecapture;
+
+public class DriverDefinition {
+	public static void main(String[] args) {
+		//EMpty class
+		
+	}
+
+}
---
+++ src/test/java/fullpagecapture/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ravio
+ *
+ */
+package fullpagecapture;
