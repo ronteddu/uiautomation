523637f51c1b1e9b64f357d639af282de7482ea3
Ravi Onteddu <ravio@Ravis-MacBook-Pro.local>
2018-02-20 20:56:41 +1100

    Initial commit barebones

---
+++ uiautomate/src/main/java/uiautomate/uiautomate/App.java
@@ -0,0 +1,13 @@
+package uiautomate.uiautomate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
